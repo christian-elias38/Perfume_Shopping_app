03afa3b9058bf796a87c105a1da0b4eaf7a4e468
chr1133 <christianelias102@gmail.com>
2026-09-01 19:12:41 +0300

    feat: implement reusable UI components including category pills, custom buttons, and luxury banners

--- lib/widgets/category_pill.dart
+++ lib/widgets/category_pill.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../core/constants/app_colors.dart';
 import '../models/category_model.dart';
 
-class CategoryPill extends StatelessWidget {
+class CategoryPill extends StatefulWidget {
   final CategoryModel category;
   final bool isSelected;
   final VoidCallback onTap;
@@ -15,54 +15,81 @@ class CategoryPill extends StatelessWidget {
   });
 
   @override
+  State<CategoryPill> createState() => _CategoryPillState();
+}
+
+class _CategoryPillState extends State<CategoryPill> {
+  bool _isHovered = false;
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            width: 62,
-            height: 62,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: isSelected ? AppColors.primary : AppColors.surfaceVariant,
-              border: Border.all(
-                color: isSelected ? AppColors.accentGold : AppColors.border,
-                width: isSelected ? 2 : 1,
-              ),
-              boxShadow: isSelected
-                  ? [
+    final isSel = widget.isSelected;
+
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AnimatedScale(
+              scale: _isHovered ? 1.10 : 1.0,
+              duration: const Duration(milliseconds: 200),
+              curve: Curves.easeOutCubic,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                width: 64,
+                height: 64,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: isSel ? AppColors.primary : AppColors.surfaceVariant,
+                  border: Border.all(
+                    color: isSel
+                        ? AppColors.accentGold
+                        : (_isHovered ? AppColors.borderGold : AppColors.border),
+                    width: isSel ? 2.5 : (_isHovered ? 2.0 : 1.0),
+                  ),
+                  boxShadow: [
+                    if (isSel || _isHovered)
                       BoxShadow(
-                        color: AppColors.primary.withOpacity(0.2),
-                        blurRadius: 8,
+                        color: isSel
+                            ? AppColors.primary.withOpacity(0.35)
+                            : AppColors.accentGold.withOpacity(0.25),
+                        blurRadius: _isHovered ? 12 : 8,
+                        spreadRadius: _isHovered ? 1 : 0,
                         offset: const Offset(0, 4),
                       )
-                    ]
-                  : null,
-            ),
-            child: ClipOval(
-              child: Image.network(
-                category.imageUrl,
-                fit: BoxFit.cover,
-                errorBuilder: (_, __, ___) => Icon(
-                  Icons.spa_outlined,
-                  color: isSelected ? Colors.white : AppColors.primary,
-                  size: 28,
+                  ],
+                ),
+                child: ClipOval(
+                  child: Image.network(
+                    widget.category.imageUrl,
+                    fit: BoxFit.cover,
+                    errorBuilder: (_, __, ___) => Icon(
+                      Icons.spa_outlined,
+                      color: isSel ? Colors.white : AppColors.primary,
+                      size: 28,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 6),
-          Text(
-            category.name,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: isSelected ? FontWeight.bold : FontWeight.w500,
-              color: isSelected ? AppColors.primary : AppColors.textSecondary,
+            const SizedBox(height: 7),
+            AnimatedDefaultTextStyle(
+              duration: const Duration(milliseconds: 200),
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: isSel || _isHovered ? FontWeight.bold : FontWeight.w500,
+                color: isSel
+                    ? AppColors.primary
+                    : (_isHovered ? AppColors.primaryLight : AppColors.textSecondary),
+              ),
+              child: Text(widget.category.name),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
